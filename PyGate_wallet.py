b1ef3ec8c3da6e60dece5359b2bf7cfe11cc68f1
cynthiahmaina16-cyber <cynthiahmaina16@gmail.com>
2026-09-22 14:05:36 +0300

    Update PyGate_wallet.py

--- PyGate_wallet.py
+++ PyGate_wallet.py
@@ -1,44 +1,36 @@
+User_name = input("Enter username: ")
 
-while True:
-    # 1. Display the choices to the user
-    print("\n--- Simple Calculator ---")
-    print("Select an operation:")
-    print("1. Sum (+)")
-    print("2. Subtract (-)")
-    print("3. Multiply (*)")
-    print("4. Divide (/)")
-
-    choice = input("Enter choice (1, 2, 3, or 4): ")
-
-
-    if choice in ['1', '2', '3', '4']:
+if User_name == "admin":
+    print("Welcome, Administrator!\n")
+    
+    attempts = 0
+    access_granted = False
+    
+    while attempts < 3:
+        correct_password = input("Enter Password: ")
         
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            result = num1 + num2
-            print(f"Result: {num1} + {num2} = {result}")
-
-        elif choice == '2':
-            result = num1 - num2
-            print(f"Result: {num1} - {num2} = {result}")
-
-        elif choice == '3':
-            result = num1 * num2
-            print(f"Result: {num1} * {num2} = {result}")
-
-        elif choice == '4':
-            if num2 == 0:
-                print("Error: You cannot divide by zero!")
-            else:
-                result = num1 / num2
-                print(f"Result: {num1} / {num2} = {result}")
+        if correct_password == "Collins2013.":
+            print(" Access Granted!\n")
+            access_granted = True
+            break  
+        else:
+            attempts += 1  
+            remaining_tries = 3 - attempts
+            print(f" Wrong password! Tries remaining: {remaining_tries}\n")
+            
+    if access_granted:
+        account_balance = 5000
+        print(f"Current Wallet Balance: KSh {account_balance}")
+        withdrawal_amount = int(input("Enter amount to withdraw: "))
+        
+        if withdrawal_amount > account_balance:
+            print(" TRANSACTION DENIED: Insufficient funds!")
+        else:
+            remaining_balance = account_balance - withdrawal_amount
+            print("Transaction successful!")
+            print(f"Remaining Balance: KSh {remaining_balance}")
     else:
-        print("Invalid choice. Please pick a number from 1 to 4.")
+        print("ACCOUNT LOCKED: Too many failed password attempts.")
 
-   
-    repeat = input("\nDo you want to do another calculation? (y/n): ").lower()
-    if repeat != 'y':
-        print("Goodbye!")
-        break 
+else:
+    print(" Access denied. Unknown user profile. Financial access blocked.")
